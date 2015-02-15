a1055ee86d9ce2ab37f103fbe8712ae874b0518c
lab <lab@wm320-a229578.campus.ithaca.lan>
2015-02-15 16:25:51 -0500

    first ui changes

---
+++ IOS Music App 2.0/ThirdViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ThirdViewController.swift
+//  IOS Music App 2.0
+//
+//  Created by lab on 2/15/15.
+//  Copyright (c) 2015 Kaloyan Popzlatev. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
